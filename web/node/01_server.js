338266a10424a3d6be928282a70ea6502ae91afc
monotonewang <zgjxfuqingwang@gmail.com>
2020-10-30 11:41:12 +0800

    正整数

--- web/node/01_server.js
+++ web/node/01_server.js
@@ -1,14 +1,29 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+let a = [1, 2, 3];
+
+a = a.map(item => {
+  item = item + 1;
+  return item;
+})
+
+console.log(a);
+let num = 0;
+if (!(/(^[1-9]\d*$)/.test(num))) {
+  console.log("输入的不是正整数");
+} else {
+  console.log("输入的是正整数");
+} 
